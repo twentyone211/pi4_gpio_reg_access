46fce8e019c28f6ac1062d562ca5db4fae7b909a
owner <s0549557@htw-berlin.de>
2021-01-08 22:05:00 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,104 @@
+
+
+#define BCM2711_PERI_BASE        (0x7E000000)
+#define GPIO_BASE                (0x7E215000) /* GPIO controller  page 87 rpi_DATA_2711_1p0.pdf */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+int  mem_fd;
+char *gpio_mem, *gpio_map;
+char *spi0_mem, *spi0_map;
+
+
+// I/O access
+volatile unsigned *gpio;
+
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+void setup_io();
+
+int main(int argc, char **argv)
+{ 
+   int i=0;
+   setup_io();
+
+   for (i=0; i<32 ; i++)
+   { 
+      printf("%i:\nvalue: 0x%08x\nReg:0x%08x\n\n", i+1,*(gpio+i), (gpio+i));
+   }
+
+   printf("\n\n\n\n");
+
+      for (i=0; i<32 ; i++)
+   { 
+      *(gpio+i) = 0x00000000; //clear all register
+      printf("%i:\nvalue: 0x%08x\nReg:0x%08x\n\n",i+1, *(gpio+i), (gpio+i));
+   }
+  
+
+  return 0;
+
+} 
+
+
+
+void setup_io()
+{
+   /* open /dev/mem */
+   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+      printf("can't open /dev/mem \n");
+      exit (-1);
+   }
+
+   /* mmap GPIO */
+
+   // Allocate MAP block
+   if ((gpio_mem = malloc(BLOCK_SIZE + (PAGE_SIZE-1))) == NULL) {
+      printf("allocation error \n");
+      exit (-1);
+   }
+
+   // Make sure pointer is on 4K boundary
+   if ((unsigned long)gpio_mem % PAGE_SIZE)
+     gpio_mem += PAGE_SIZE - ((unsigned long)gpio_mem % PAGE_SIZE);
+
+   // Now map it
+   gpio_map = (unsigned char *)mmap(
+      (caddr_t)gpio_mem,
+      BLOCK_SIZE,
+      PROT_READ|PROT_WRITE,
+      MAP_SHARED|MAP_FIXED,
+      mem_fd,
+      GPIO_BASE
+   );
+
+   if ((long)gpio_map < 0) {
+      printf("mmap error %d\n", (int)gpio_map);
+      exit (-1);
+   }
+ 
+   gpio = (volatile unsigned *)gpio_map;
+
+
+} 
